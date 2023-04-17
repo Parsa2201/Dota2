3d2095999b4213856eae81aa04916971aca1c7bb
Parsa Salamatipour <sa.parsa20@gmail.com>
2023-04-17 20:30:28 +0330

    first commit on core branch

--- desktop/src/com/ap/dota2/DesktopLauncher.java
+++ desktop/src/com/ap/dota2/DesktopLauncher.java
@@ -4,8 +4,10 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.ap.dota2.MyGdxGame;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
+public class DesktopLauncher
+{
+	public static void main(String[] arg)
+	{
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Dota2");
