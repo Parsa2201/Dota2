591130e2600e244b14a74714621460e2204f5687
Parsa Salamatipour <sa.parsa20@gmail.com>
2023-04-18 21:27:53 +0330

    test with behrooz

--- desktop/src/com/ap/dota2/DesktopLauncher.java
+++ desktop/src/com/ap/dota2/DesktopLauncher.java
@@ -10,7 +10,7 @@ public class DesktopLauncher
 	{
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("Dota2");
+		config.setTitle("Dota3s");
 		config.useVsync(true);
 		new Lwjgl3Application(new MyGdxGame(), config);
 	}
