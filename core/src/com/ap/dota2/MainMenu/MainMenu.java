c91555f54813f78fa8061909cd3e3071e4aa1cb3
Sina Sahabi <sinasahabi1@gmail.com>
2023-04-23 02:11:26 +0330

    add new button, makeNewButton method, etc

--- core/src/com/ap/dota2/MainMenu/MainMenu.java
+++ core/src/com/ap/dota2/MainMenu/MainMenu.java
@@ -8,10 +8,12 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
+import com.badlogic.gdx.utils.Null;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
@@ -26,6 +28,28 @@ public class MainMenu implements Screen
 	private final Stage stage;
 	private final Table table;
 
+	/**
+	 * create a new button (first edition - TextButton only)
+	 * @param Image
+	 * @param massage
+	 * @param color
+	 * @return Button
+	 */
+	public static Button makeNewButton (String Image, @Null String massage, @Null Color color) {
+		NinePatch patch = new NinePatch(new Texture(Image), 12, 12, 12, 12);
+		
+		BitmapFont myFont = new BitmapFont();
+		
+		NinePatchDrawable drawable = new NinePatchDrawable(patch);
+
+		TextButtonStyle myTextButtonStyle = new TextButtonStyle(drawable, drawable, drawable, myFont);
+		if (color != null) myTextButtonStyle.fontColor = color;
+		else myTextButtonStyle.fontColor = Color.RED;
+		
+		TextButton myButton = new TextButton(massage, myTextButtonStyle);
+		return myButton;
+	}
+	
 	/**
 	 * Constructor for the main menu.
 	 * @param game The game object.
@@ -42,8 +66,7 @@ public class MainMenu implements Screen
 
 		table.setDebug(true); // This is optional, but enables debug lines for tables.
 
-		// TODO: Add buttons to the table.
-
+		//!
 		NinePatch patch = new NinePatch(new Texture("button1.png"), 12, 12, 12, 12);
 		
 		BitmapFont myFont = new BitmapFont();
@@ -56,6 +79,10 @@ public class MainMenu implements Screen
 		TextButton myButton = new TextButton("hello bluh bluh bluh bluh bluh bluh bluh bluh world", myTextButtonStyle);
 
 		table.add(myButton);
+		///
+
+		table.row(); //!
+		table.add(makeNewButton("button3.png", "helloooooooooooooooooooo world", Color.BLUE));
 	}
 
 	@Override
