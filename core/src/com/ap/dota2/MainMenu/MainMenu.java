7386b4a9839a29772eccf35b1251b4f183574808
Sina Sahabi <sinasahabi1@gmail.com>
2023-04-22 16:47:35 +0330

    add button (has error)

--- core/src/com/ap/dota2/MainMenu/MainMenu.java
+++ core/src/com/ap/dota2/MainMenu/MainMenu.java
@@ -4,86 +4,96 @@ import com.ap.dota2.Dota2Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
 /**
  * The main menu screen.
- * @author Parsa Salamatipour
+ * @author Parsa Salamatipour, Sina Sahabi
  * @version 1.0
  */
 public class MainMenu implements Screen
 {
-    private final Dota2Game game;
-    private final Stage stage;
-    private final Table table;
+	private final Dota2Game game;
+	private final Stage stage;
+	private final Table table;
 
-    /**
-     * Constructor for the main menu.
-     * @param game The game object.
-     */
-    public MainMenu(Dota2Game game)
-    {
-        this.game = game;
-        this.stage = new Stage(new ExtendViewport(1920, 1080));
-        Gdx.input.setInputProcessor(stage);
+	/**
+	 * Constructor for the main menu.
+	 * @param game The game object.
+	 */
+	public MainMenu(Dota2Game game)
+	{
+		this.game = game;
+		this.stage = new Stage(new ExtendViewport(1920, 1080));
+		Gdx.input.setInputProcessor(stage);
 
-        this.table = new Table();
-        table.setFillParent(true);
-        stage.addActor(table);
+		this.table = new Table();
+		table.setFillParent(true);
+		stage.addActor(table);
 
-        table.setDebug(true); // This is optional, but enables debug lines for tables.
+		table.setDebug(true); // This is optional, but enables debug lines for tables.
 
-        // TODO: Add buttons to the table.
-    }
+		// TODO: Add buttons to the table.
 
-    @Override
-    public void show()
-    {
+		NinePatch patch = new NinePatch(new Texture(Gdx.files.internal("knob.png")), 12, 12, 12, 12);
+		
+		// TextButtonStyle style = new TextButtonStyle(patch, patch, patch, 0, 0, 0, 0, new BitmapFont(), new Color(0.3f, 0.2f, 0.8f, 1f), new Color(0, 0, 0, 1f), new Color(0, 0, 0, 1f));
+		
+		TextButton myButton = new TextButton("hello world", style);
+	}
 
-    }
+	@Override
+	public void show()
+	{
 
-    @Override
-    public void render(float delta)
-    {
-        // clear the screen with the given RGB color (black)
-        Gdx.gl.glClear(Gdx.gl.GL_COLOR_BUFFER_BIT);
-        ScreenUtils.clear(Color.BLACK);
+	}
 
-        // tell the stage to do actions and draw itself
-        stage.act(delta);
-        stage.draw();
-    }
+	@Override
+	public void render(float delta)
+	{
+		// clear the screen with the given RGB color (black)
+		Gdx.gl.glClear(Gdx.gl.GL_COLOR_BUFFER_BIT);
+		ScreenUtils.clear(Color.BLACK);
 
-    @Override
-    public void resize(int width, int height)
-    {
-        stage.getViewport().update(width, height, true);
-    }
+		// tell the stage to do actions and draw itself
+		stage.act(delta);
+		stage.draw();
+	}
 
-    @Override
-    public void pause()
-    {
+	@Override
+	public void resize(int width, int height)
+	{
+		stage.getViewport().update(width, height, true);
+	}
 
-    }
+	@Override
+	public void pause()
+	{
 
-    @Override
-    public void resume()
-    {
+	}
 
-    }
+	@Override
+	public void resume()
+	{
 
-    @Override
-    public void hide()
-    {
+	}
 
-    }
+	@Override
+	public void hide()
+	{
 
-    @Override
-    public void dispose()
-    {
-        stage.dispose();
-    }
+	}
+
+	@Override
+	public void dispose()
+	{
+		stage.dispose();
+	}
 }
