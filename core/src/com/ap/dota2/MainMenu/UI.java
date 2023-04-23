b74a6c6dcebe48dd37a59cd98415d02e7f09f3c0
Sina Sahabi <sinasahabi1@gmail.com>
2023-04-23 13:22:34 +0330

    change indent to tab

--- core/src/com/ap/dota2/MainMenu/UI.java
+++ core/src/com/ap/dota2/MainMenu/UI.java
@@ -17,67 +17,67 @@ import com.badlogic.gdx.utils.Null;
 
 public class UI
 {
-    private final MainMenu mainMenu;
-    private final Stage stage;
-    private final Table rootTable;
+	private final MainMenu mainMenu;
+	private final Stage stage;
+	private final Table rootTable;
 
-    public UI(MainMenu mainMenu, Stage stage, Table table)
-    {
-        this.mainMenu = mainMenu;
-        this.stage = stage;
-        this.rootTable = table;
-        rootTable.setFillParent(true);
-        stage.addActor(rootTable);
-        Gdx.input.setInputProcessor(stage);
+	public UI(MainMenu mainMenu, Stage stage, Table table)
+	{
+		this.mainMenu = mainMenu;
+		this.stage = stage;
+		this.rootTable = table;
+		rootTable.setFillParent(true);
+		stage.addActor(rootTable);
+		Gdx.input.setInputProcessor(stage);
 
-        table.setDebug(Dota2Game.DEBUG); // This is optional, but enables debug lines for tables.
-    }
+		table.setDebug(Dota2Game.DEBUG); // This is optional, but enables debug lines for tables.
+	}
 
-    /**
-     * initialize the UI
-     */
-    public void init()
-    {
-        //!
-        NinePatch patch = new NinePatch(new Texture("button1.png"), 12, 12, 12, 12);
+	/**
+	 * initialize the UI
+	 */
+	public void init()
+	{
+		//!
+		NinePatch patch = new NinePatch(new Texture("button1.png"), 12, 12, 12, 12);
 
-        BitmapFont myFont = new BitmapFont();
+		BitmapFont myFont = new BitmapFont();
 
-        NinePatchDrawable drawable = new NinePatchDrawable(patch);
+		NinePatchDrawable drawable = new NinePatchDrawable(patch);
 
-        TextButton.TextButtonStyle myTextButtonStyle = new TextButton.TextButtonStyle(drawable, drawable, drawable, myFont);
-        myTextButtonStyle.fontColor = Color.RED;
+		TextButton.TextButtonStyle myTextButtonStyle = new TextButton.TextButtonStyle(drawable, drawable, drawable, myFont);
+		myTextButtonStyle.fontColor = Color.RED;
 
-        TextButton myButton = new TextButton("hello bluh bluh bluh bluh bluh bluh bluh bluh world", myTextButtonStyle);
+		TextButton myButton = new TextButton("hello bluh bluh bluh bluh bluh bluh bluh bluh world", myTextButtonStyle);
 
-        rootTable.add(myButton);
-        ///
+		rootTable.add(myButton);
+		///
 
-        rootTable.row(); //!
-        rootTable.add(makeNewButton("button3.png", "helloooooooooooooooooooo world", Color.BLUE));
-    }
+		rootTable.row(); //!
+		rootTable.add(makeNewButton("button3.png", "helloooooooooooooooooooo world", Color.BLUE));
+	}
 
-    /**
-     * create a new button (first edition - TextButton only)
-     * @param Image - the image of the button
-     * @param massage - the massage of the button
-     * @param color - the color of the button
-     * @return Button
-     */
-    public static Button makeNewButton (String Image, @Null String massage, @Null Color color)
-    {
-        NinePatch patch = new NinePatch(new Texture(Image), 12, 12, 12, 12);
+	/**
+	 * create a new button (first edition - TextButton only)
+	 * @param Image - the image of the button
+	 * @param massage - the massage of the button
+	 * @param color - the color of the button
+	 * @return Button
+	 */
+	public static Button makeNewButton (String Image, @Null String massage, @Null Color color)
+	{
+		NinePatch patch = new NinePatch(new Texture(Image), 12, 12, 12, 12);
 
-        BitmapFont myFont = new BitmapFont();
+		BitmapFont myFont = new BitmapFont();
 
-        NinePatchDrawable drawable = new NinePatchDrawable(patch);
+		NinePatchDrawable drawable = new NinePatchDrawable(patch);
 
-        TextButton.TextButtonStyle myTextButtonStyle = new TextButton.TextButtonStyle(drawable, drawable, drawable, myFont);
-        if(color != null) myTextButtonStyle.fontColor = color;
-        else myTextButtonStyle.fontColor = Color.RED;
+		TextButton.TextButtonStyle myTextButtonStyle = new TextButton.TextButtonStyle(drawable, drawable, drawable, myFont);
+		if(color != null) myTextButtonStyle.fontColor = color;
+		else myTextButtonStyle.fontColor = Color.RED;
 
-        TextButton myButton = new TextButton(massage, myTextButtonStyle);
-        return myButton;
-    }
+		TextButton myButton = new TextButton(massage, myTextButtonStyle);
+		return myButton;
+	}
 
 }
