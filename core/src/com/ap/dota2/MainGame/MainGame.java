fecaf2b971f828a0806a8a4ad27658162b32ff25
Parsa Salamatipour <sa.parsa20@gmail.com>
2023-04-25 13:29:32 +0330

    create "MainGame" and "Map"

---
+++ core/src/com/ap/dota2/MainGame/MainGame.java
@@ -0,0 +1,56 @@
+package com.ap.dota2.MainGame;
+
+import com.ap.dota2.Dota2Game;
+import com.badlogic.gdx.Screen;
+
+public class MainGame implements Screen
+{
+    public final Dota2Game game;
+
+    public MainGame(Dota2Game game)
+    {
+        this.game = game;
+    }
+
+    @Override
+    public void show()
+    {
+
+    }
+
+    @Override
+    public void render(float delta)
+    {
+
+    }
+
+    @Override
+    public void resize(int width, int height)
+    {
+
+    }
+
+    @Override
+    public void pause()
+    {
+
+    }
+
+    @Override
+    public void resume()
+    {
+
+    }
+
+    @Override
+    public void hide()
+    {
+
+    }
+
+    @Override
+    public void dispose()
+    {
+
+    }
+}
---
+++ core/src/com/ap/dota2/MainGame/Map.java
@@ -0,0 +1,93 @@
+package com.ap.dota2.MainGame;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+
+public class Map implements Drawable
+{
+    Texture background;
+
+    public Map()
+    {
+
+    }
+
+    @Override
+    public void draw(Batch batch, float x, float y, float width, float height)
+    {
+
+    }
+
+    @Override
+    public float getLeftWidth()
+    {
+        return 0;
+    }
+
+    @Override
+    public void setLeftWidth(float leftWidth)
+    {
+
+    }
+
+    @Override
+    public float getRightWidth()
+    {
+        return 0;
+    }
+
+    @Override
+    public void setRightWidth(float rightWidth)
+    {
+
+    }
+
+    @Override
+    public float getTopHeight()
+    {
+        return 0;
+    }
+
+    @Override
+    public void setTopHeight(float topHeight)
+    {
+
+    }
+
+    @Override
+    public float getBottomHeight()
+    {
+        return 0;
+    }
+
+    @Override
+    public void setBottomHeight(float bottomHeight)
+    {
+
+    }
+
+    @Override
+    public float getMinWidth()
+    {
+        return 0;
+    }
+
+    @Override
+    public void setMinWidth(float minWidth)
+    {
+
+    }
+
+    @Override
+    public float getMinHeight()
+    {
+        return 0;
+    }
+
+    @Override
+    public void setMinHeight(float minHeight)
+    {
+
+    }
+}
