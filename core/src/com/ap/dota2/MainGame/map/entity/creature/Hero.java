7098a086e8cc2f607127b195ac3ab446aa74ea7a
Sina Sahabi <sinasahabi1@gmail.com>
2023-05-25 16:06:27 +0330

    movement, issue

--- core/src/com/ap/dota2/MainGame/map/entity/creature/Hero.java
+++ core/src/com/ap/dota2/MainGame/map/entity/creature/Hero.java
@@ -1,6 +1,7 @@
 package com.ap.dota2.MainGame.map.entity.creature;
 
 import com.ap.dota2.MainGame.standards.Direction;
+import com.ap.dota2.MainGame.standards.Position;
 import com.ap.dota2.MainGame.standards.Velocity;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Input.Buttons;
@@ -11,6 +12,7 @@ public class Hero extends Creature
 {
     private Texture texture;
     private Direction direction;
+    private Movement movement;
 
     public Hero(int x, int y)
     {
@@ -19,7 +21,11 @@ public class Hero extends Creature
         velocity.setX(800);
         velocity.setY(800);
         direction = Direction.NONE;
+        movement = new Movement(this);
+        movement.start();
     }
+
+    public Position getPosition() { return position; }
 
     @Override
     public void draw(Batch batch)
@@ -52,6 +58,117 @@ public class Hero extends Creature
                 break;
         }
     }
+
+    /*
+    final float eps = 0.1f;
+    public void move(float dx, float dy) {
+        if (dx >= 0f) {
+            if (dy >= 0f) {
+                while (dx >= eps && dy >= eps) {
+                    dx -= eps; dy -= eps;
+                    direction = Direction.DOWN;
+                    move(eps);
+                    direction = Direction.RIGHT;
+                    move(eps);
+                }
+                while (dx >= eps) {
+                    dx -= eps;
+                    direction = Direction.DOWN;
+                    move(eps);
+                }
+                while (dy >= eps) {
+                    dy -= eps;
+                    direction = Direction.RIGHT;
+                    move(eps);
+                }
+                direction = Direction.DOWN;
+                if (dx < 0f) dx *= -1f;
+                move(dx);
+                direction = Direction.RIGHT;
+                if (dy < 0f) dy *= -1f;
+                move(dy);
+                direction = Direction.NONE;
+            } else {
+                while (dx >= eps && dy <= eps) {
+                    dx -= eps; dy += eps;
+                    direction = Direction.DOWN;
+                    move(eps);
+                    direction = Direction.LEFT;
+                    move(eps);
+                }
+                while (dx >= eps) {
+                    dx -= eps;
+                    direction = Direction.DOWN;
+                    move(eps);
+                }
+                while (dy <= eps) {
+                    dy += eps;
+                    direction = Direction.LEFT;
+                    move(eps);
+                }
+                direction = Direction.DOWN;
+                if (dx < 0f) dx *= -1f;
+                move(dx);
+                direction = Direction.LEFT;
+                if (dy < 0f) dy *= -1f;
+                move(dy);
+                direction = Direction.NONE;
+            }
+        } else {
+            if (dy >= 0f) {
+                while (dx <= eps && dy >= eps) {
+                    dx += eps; dy -= eps;
+                    direction = Direction.UP;
+                    move(eps);
+                    direction = Direction.RIGHT;
+                    move(eps);
+                }
+                while (dx <= eps) {
+                    dx += eps;
+                    direction = Direction.UP;
+                    move(eps);
+                }
+                while (dy >= eps) {
+                    dy -= eps;
+                    direction = Direction.RIGHT;
+                    move(eps);
+                }
+                direction = Direction.UP;
+                if (dx < 0f) dx *= -1f;
+                move(dx);
+                direction = Direction.RIGHT;
+                if (dy < 0f) dy *= -1f;
+                move(dy);
+                direction = Direction.NONE;
+            } else {
+                while (dx <= eps && dy <= eps) {
+                    dx += eps; dy += eps;
+                    direction = Direction.UP;
+                    move(eps);
+                    direction = Direction.LEFT;
+                    move(eps);
+                }
+                while (dx <= eps) {
+                    dx += eps;
+                    direction = Direction.DOWN;
+                    move(eps);
+                }
+                while (dy <= eps) {
+                    dy += eps;
+                    direction = Direction.LEFT;
+                    move(eps);
+                }
+                direction = Direction.UP;
+                if (dx < 0f) dx *= -1f;
+                move(dx);
+                direction = Direction.LEFT;
+                if (dy < 0f) dy *= -1f;
+                move(dy);
+                direction = Direction.NONE;
+            }
+        }
+    }
+    */
 
     @Override
     public void attack()
@@ -98,10 +215,38 @@ public class Hero extends Creature
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button)
+    public synchronized boolean touchDown(int screenX, int screenY, int pointer, int button)
     {
         if (button == Buttons.LEFT) {
-            
+            // move((float)(screenX - position.getX()), (float)(screenY - position.getY()));
+            // int dx = (screenX - position.getX()), dy = -(screenY - position.getY());
+            // while (dx != 0 && dy != 0) {
+            //     keyDown(InputKeys(Xdir(dx))); dx = moveToZero(dx);
+            //     keyDown(InputKeys(Ydir(dy))); dy = moveToZero(dy);
+            // }
+            // while (dx != 0) {
+            //     keyDown(InputKeys(Xdir(dx))); dx = moveToZero(dx);
+            // }
+            // while (dy != 0) {
+            //     keyDown(InputKeys(Ydir(dy))); dy = moveToZero(dy);
+            // }
+
+            // while (screenX != position.getX() && screenY != position.getY()) {
+            //     keyDown(InputKeys(Xdir(dx)));
+            //     keyDown(InputKeys(Ydir(dy)));
+            // }
+            // while (screenX != position.getX()) {
+            //     keyDown(InputKeys(Xdir(dx)));
+            // }
+            // while (screenY != position.getY()) {
+            //     keyDown(InputKeys(Ydir(dy)));
+            // }
+            // try {
+            //     if (movement.isAlive()) movement.stop();
+            // } catch (Exception e) {}
+            movement.Xtarget = screenX; movement.Ytarget = screenY;
+            // movement.start();
+
             return true;
         }
         return false;
